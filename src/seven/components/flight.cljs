c6cba21b3f5d7e2efc7b40db3964b526ee8f0f8f
jonathan dannel <usixjad@gmail.com>
2021-01-10 02:44:42 -0500

    try parsing dates

--- src/seven/components/flight.cljs
+++ src/seven/components/flight.cljs
@@ -1,10 +1,11 @@
 (ns seven.components.flight
   (:require [reagent.core :as r]
-            [seven.components.ui :refer [component-wrapper]]))
+            [seven.components.ui :refer [component-wrapper]]
+            [clojure.string :as s]))
 
 (def opts {:1 "One way flight" :2 "Round trip flight"})
 
-(defonce state (r/atom {:active-option 1 :errors [2 5] :depart nil :return nil}))
+(defonce state (r/atom {:active-option 1 :errors [2 5] :depart-date "" :return-date ""}))
 
 (defn handle-select [e]
   (let [v (-> e .-target .-value)]
@@ -12,7 +13,26 @@
 
 (defn handle-date-change [e]
   (let [k (-> e .-target .-name) v (-> e .-target .-value)]
-    (print k v)))
+    (swap! state assoc (keyword k)  v)))
+
+(defn parse-date [v] 
+    (let [dmy (s/split v #"/")]
+      
+      
+      
+      ))
+    
+
+(defn 
+
+;4th argument of callback is new value
+(add-watch state :validator
+  (fn [_ _ _ a] 
+    (print a)))
+  
+
+
+
 
 (defn main []
   [component-wrapper "Flight booker"
@@ -27,8 +47,8 @@
     [:div {:class "field"}
      [:label {:class "label"} "Depart"]
      [:div {:class "control"}
-      [:input {:class "input" :on-change handle-date-change  :name "depart" :type "text" :placeholder "ex: 02/31/2021"}]]]
+      [:input {:class "input" :value (@state :depart-date) :on-change handle-date-change :name "depart-date" :type "text" :placeholder "ex: 02/31/2021"}]]]
     [:div {:class "field"}
      [:label {:class "label"} "Return"]
      [:div {:class "control"}
-      [:input {:class "input" :on-change handle-date-change :name "return"  :disabled (seq (:errors @state)) :type "text" :placeholder "ex: 03/08/2021"}]]]]])
+      [:input {:class "input" :value (@state :return-date) :on-change handle-date-change :name "return-date"  :disabled (seq (:errors @state)) :type "text" :placeholder "ex: 03/08/2021"}]]]]])
