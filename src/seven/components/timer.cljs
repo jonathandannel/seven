cca26f55862912b30127d724ea88aafc5ae6d2c3
jonathan dannel <usixjad@gmail.com>
2021-01-13 14:56:33 -0500

    finish timer, start on crud

--- src/seven/components/timer.cljs
+++ src/seven/components/timer.cljs
@@ -2,40 +2,34 @@
   (:require [reagent.core :as r]
             [seven.components.ui :refer [component-wrapper]]))
 
-(defonce state (r/atom {:elapsed-duration 0 :chosen-duration 15 :max-duration 32}))
+(defonce state (r/atom {:elapsed-duration 0 :chosen-duration 60 :max-duration 180}))
 (defonce interval-object (r/atom nil))
-(defonce timer-running (r/atom false))
 
 (defn handle-duration-change [e]
   (let [new-duration (-> e .-target .-value)]
-    (swap! state assoc [:chosen-duration] new-duration)
-    (print new-duration)))
+    (swap! state assoc :chosen-duration new-duration)))
 
-; Assign the interval object to an atom so we can clear it when we need to
+; Assign the js interval object to an atom so we can clear it when we need to
 (defn set-interval []
   ; Clear interval if it exists to avoid weird behavior
-  (if (not= @interval-object nil) (js/clearInterval @interval-object))
+  (if interval-object (js/clearInterval @interval-object))
   (reset! interval-object
           (js/setInterval
-           ; Add a second to elapsed duration on every tick
-           #(swap! state update-in [:elapsed-duration] inc) 1000)))
+           ; Inc 1 second to elapsed duration on every tick
+           #(swap! state update :elapsed-duration inc) 1000)))
 
 (defn reset-timer []
   ; Clear accrued time and set a new interval
-  (reset! (state :elapsed-duration) 0)
+  (swap! state assoc :elapsed-duration 0)
   (set-interval))
-
-(add-watch state :state-watcher
-           (fn [k a o n] (print n)))
 
 ; Start timer on mount
 (set-interval)
 
 (defn main []
   [component-wrapper "Timer"
-   ; Auto run on mount
    [:div {:class "content" :style {:height "fit-content"}}
-    [:progress {:class "progress is-primary" :value "33" :max "66"}]
+    [:progress {:class "progress is-primary" :value (str (@state :elapsed-duration)) :max (str (@state :chosen-duration))}]
     [:div {:class "block"}]
     [:span (@state :elapsed-duration) " seconds elapsed"]
     [:div {:class "block"}]
@@ -43,11 +37,10 @@
              :on-change handle-duration-change
              :style {:width "100%"}
              :step 1
-             :value (@state [:chosen-duration])
              :min 0
              :max (@state :max-duration)}]
 
     [:div {:class "block"}]
     [:span (@state :chosen-duration) " seconds maximum"]
     [:div {:class "block"}]
-    [:button {:class "button is-primary" :on-click set-interval} "Reset"]]])
+    [:button {:class "button is-primary" :on-click reset-timer} "Reset"]]])
