2e3793404beb4d35faeb59029b0d706df07d00c3
jonathan dannel <usixjad@gmail.com>
2021-01-13 06:15:41 -0500

    clear invalid state properly

--- src/seven/components/flight.cljs
+++ src/seven/components/flight.cljs
@@ -16,8 +16,9 @@
 (def depart-cursor (r/cursor state [:depart-date :value]))
 (def return-cursor (r/cursor state [:return-date :value]))
 
-(defn update-error [k err bool]
-  (swap! state assoc-in [(keyword k) :errors (keyword err)] bool))
+; Update error based on :depart-date or :return-date fields 
+(defn update-error [field err bool]
+  (swap! state assoc-in [(keyword field) :errors (keyword err)] bool))
 
 ; Set error fields true or false on input state change
 (defn check-errors [field value]
@@ -25,14 +26,16 @@
     (update-error field :chars char-err)
     (update-error field :format format-err)
     (if
-     (and (not (or char-err format-err)) (> (count value) 0))
+     (not (or char-err format-err (< (count value) 1)))
       (cond
         (= field :depart-date)
         (update-error field :invalid (helpers/bad-start-date? value))
         (= field :return-date)
-        (update-error field :invalid (helpers/bad-return-date? value))))))
+        (update-error field :invalid (helpers/bad-return-date? @depart-cursor value)))
+      ; Remove the invalid state when date isn't fully entered again
+      (update-error field :invalid false))))
 
-; Pass changed state values to `check-errors`
+; Pass changed state values to `check-errors`, %4th argument is new state after change
 (add-watch depart-cursor :depart-watcher #(check-errors :depart-date %4))
 (add-watch return-cursor :return-watcher #(check-errors :return-date %4))
 
@@ -47,7 +50,6 @@
       (= k "depart") (reset! depart-cursor v)
       (= k "return") (reset! return-cursor v))))
 
-; Component
 (defn main []
   [component-wrapper "Flight booker"
    [:div {:class "content"}
@@ -61,10 +63,14 @@
     [:div {:class "field"}
      [:label {:class "label"} "Depart"]
      [:div {:class "control"}
-      (and (-> @state :depart-date :errors :format) [:span "Bad format"])
-      (and (-> @state :depart-date :errors :chars) [:span "letters disallowed"])
+      (and (-> @state :depart-date :errors :format) [:p {:class "help is-danger"} "Date must be in MM/DD/YYYY format"])
+      (and (-> @state :depart-date :errors :chars) [:p {:class "help is-danger"} "Date may only contain numbers and slashes"])
+      (and (-> @state :depart-date :errors :invalid) [:p {:class "help is-danger"} "Depart date must be in the future"])
       [:input {:class "input" :value (-> @state :depart-date :value) :on-change handle-date-change :name "depart" :type "text" :placeholder "ex: 02/31/2021"}]]]
     [:div {:class "field"}
      [:label {:class "label"} "Return"]
      [:div {:class "control"}
+      (and (-> @state :return-date :errors :format) [:p {:class "help is-danger"} "Date must be in MM/DD/YYYY format"])
+      (and (-> @state :return-date :errors :chars) [:p {:class "help is-danger"} "Date may only contain numbers and slashes"])
+      (and (-> @state :return-date :errors :invalid) [:p {:class "help is-danger"} "Return date must be after depart date"])
       [:input {:class "input" :value (-> @state :return-date :value) :on-change handle-date-change :name "return" :disabled (= (@state :active-option) 1) :type "text" :placeholder "ex: 03/08/2021"}]]]]])
--- src/seven/components/helpers.cljs
+++ src/seven/components/helpers.cljs
@@ -14,26 +14,23 @@
      (not (= (map count (split-date v)) '(2 2 4)))
      (not (= ((frequencies v) (char 47)) 2)))))
 
-;(defn bad-start-date? [d]
-  ;(let [today (js/Date.) [day month year] (split-date d)]
-    ;(print (.toDateString today) "today")
-    ;(print (js/Date. year (- month 1) day))))
-
 (defn bad-start-date? [d]
   (let [today (js/Date.) [month day year] (map int (split-date d))]
     (let [this-year (.getUTCFullYear today) this-month (.getUTCMonth today) this-day (.getUTCDate today)]
       (cond
-        (or (< 1 day) (< 1 month)) true
+        (or (< day 1) (< month 1)) true
         (< year this-year) true
-        ; Inc the month, javascript months start at zero :|
-        (and (< month (inc this-month)) (not (> year this-year))) true
-        (and (= month (inc this-month)) (< day this-day)) true))))
+        ; Inc the month, javascript months are zero based :/
+        (and (< month (inc this-month)) (< year this-year)) true
+        (and (= year this-year) (and (= month (inc this-month)) (< day this-day))) true
+        :else false))))
 
+; Ye olde violation of DRY
 (defn bad-return-date? [d1 d2]
   (let [[depart-month depart-day depart-year] (map int (split-date d1)) [return-month return-day return-year] (map int (split-date d2))]
     (cond
-      (or (< 1 return-day) (< 1 return-month)) true
+      (or (< return-day 1) (< return-month 1)) true
       (< return-year depart-year) true
-        ; Inc the month, javascript months start at zero :|
-      (and (< return-month depart-month) (not (> return-year depart-year))) true
-      (and (= return-month depart-month)) (< return-day depart-day)) true))
+      (and (< return-month depart-month) (< return-year depart-year)) true
+      (and (= return-year depart-year) (and (= return-month depart-month) (< return-day depart-day))) true
+      :else false)))
