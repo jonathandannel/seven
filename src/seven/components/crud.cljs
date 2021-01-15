a6de45ec6228cd3ebc9fc027175e631fc4e1a753
jonathan dannel <usixjad@gmail.com>
2021-01-15 02:34:12 -0500

    filtering

--- src/seven/components/crud.cljs
+++ src/seven/components/crud.cljs
@@ -4,26 +4,49 @@
 
 (defonce name-list (r/atom [{:first-name "Jonathan" :last-name "Dannel"}]))
 (defonce filter-query (r/atom ""))
+(defonce filtered-names (r/atom []))
 (defonce active-name (r/atom {:first-name "Jonathan" :last-name "Dannel"}))
+(defonce updating (r/atom {:first-name "Jonathan" :last-name "Dannel"}))
+
+(defn select-name [person]
+  (reset! active-name person)
+  (reset! updating person))
 
 (defn can-create? []
-  (not (some #(= % @active-name) name-list)))
+  (not (boolean (some #(= @active-name %) @name-list))))
 
 (defn create-entry []
   (let [first-name (@active-name :first-name)
         last-name (@active-name :last-name)]
     (if (and
-         (count first-name)
-         (count last-name)
-         (can-create?))
+         (not= first-name "")
+         (not= last-name "")
+         can-create?)
       (swap! name-list conj {:first-name first-name :last-name last-name}))))
+
+(defn update-entry []
+  (let [index (.indexOf @name-list @updating)]
+    (swap! name-list assoc index @active-name)
+    (select-name @active-name)))
+
+(defn delete-entry []
+  (let [filtered  (filterv #(not= % @active-name) @name-list)]
+    (reset! name-list filtered)
+    (reset! active-name {:first-name "" :last-name ""})
+    (reset! updating {:first-name "" :last-name ""})))
 
 (defn handle-filter-change [e]
   (reset! filter-query (-> e .-target .-value)))
 
-(add-watch name-list :name-state #(print %4))
-(add-watch filter-query :query-state #(print %4))
-(add-watch active-name :active-state #(print %4))
+
+(defn filter-entry [curr]
+  (let [query-length (count @filter-query)]
+  (if (= (subs (curr :last-name) 0 query-length) @filter-query) true)))
+
+(add-watch filter-query :query-watcher
+           (fn [k a o n]
+             (let [filtered (filterv filter-entry @name-list)]
+             (print filtered)))
 
 (defn main []
   [component-wrapper "CRUD"
@@ -41,11 +64,11 @@
       [:div.menu
        [:ul.menu-list.pr-2 {:style {:list-style-type "none" :margin 0}}
         (doall
-         (for [person @name-list]
+         (for [person (if (not= filter-query "") @name-list @filtered-names)]
            (let [first-name (person :first-name)
                  last-name (person :last-name)]
              ^{:key (str first-name last-name)}
-             [:li {:on-click #(reset! active-name person)}
+             [:li {:on-click #(select-name person)}
               [:a {:class (if (= @active-name person) "is-active")}
                first-name " " last-name]])))]]]
       ; Right side edit
@@ -65,6 +88,6 @@
            :value (@active-name :last-name)}]]]]]]
     ; Bottom buttons
     [:div.is-flex
-     [:button.button.is-primary.mr-3 {:on-click create-entry} "Create"]
-     [:button.button.is-primary.mr-3 "Update"]
-     [:button.button.is-danger.mr-3 "Delete"]]]])
+     [:button.button.is-primary.mr-3  {:disabled (not (can-create?)) :on-click create-entry} "Create"]
+     [:button.button.is-primary.mr-3 {:on-click update-entry} "Update"]
+     [:button.button.is-danger.mr-3 {:on-click delete-entry} "Delete"]]]])
