84dac437febfcd96deb3e1a6dcfd039c31f9b861
jonathan dannel <usixjad@gmail.com>
2021-01-20 01:05:36 -0500

    make a table

--- src/seven/components/spreadsheet.cljs
+++ src/seven/components/spreadsheet.cljs
@@ -2,5 +2,13 @@
   (:require [reagent.core :as r]
             [seven.components.ui :refer [component-wrapper]]))
 
+(def a-to-z (conj (map char (range 97 123)) nil))
+
 (defn main []
-  [component-wrapper "Cells"])
+  [component-wrapper "Cells"
+   [:table {:style {:overflow "scroll"}}
+    [:thead
+     [:tr
+      (map (fn [x] [:th {:key x} x]) a-to-z)]]
+    [:tbody
+     (map (fn [x] [:tr {:key x} [:th x]]) (range 26))]]])
--- src/seven/root.cljs
+++ src/seven/root.cljs
@@ -5,19 +5,21 @@
             [seven.components.timer :as timer]
             [seven.components.crud :as crud]
             [seven.components.circles :as circles]
+            [seven.components.spreadsheet :as spreadsheet]
             [seven.components.temperature :as temperature]))
 
 ;; Root contains all our components 
 (defn root []
   [:div {:class "content"}
-   [counter/main]
-   [:div {:class "block"}]
-   [temperature/main]
-   [:div {:class "block"}]
-   [flight/main]
-   [:div {:class "block"}]
-   [timer/main]
-   [:div {:class "block"}]
-   [crud/main]
-   [:div.block]
-   [circles/main]])
+   [spreadsheet/main]])
+   ;[counter/main]
+   ;[:div {:class "block"}]
+   ;[temperature/main]
+   ;[:div {:class "block"}]
+   ;[flight/main]
+   ;[:div {:class "block"}]
+   ;[timer/main]
+   ;[:div {:class "block"}]
+   ;[crud/main]
+   ;[:div.block]
+   ;[circles/main]])
