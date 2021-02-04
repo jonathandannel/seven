0e18c32495e02b12ed8317def486975aeea34aa9
jonathan dannel <usixjad@gmail.com>
2021-02-04 00:24:13 -0500

    add comment to spreadsheet, use lowercase string for query

--- src/seven/components/spreadsheet/util.cljs
+++ src/seven/components/spreadsheet/util.cljs
@@ -1,5 +1,5 @@
 (ns seven.components.spreadsheet.util
-  (:require [clojure.string :as s]))
+  (:require [clojure.string :as cs]))
 
 (def operations
   {:sum #(apply + %)
@@ -12,13 +12,13 @@
   (= (first v) "="))
 
 (defn get-coord-value [values string]
-  (let [cell (values (keyword string))]
+  (let [cell (values (keyword (cs/lower-case string)))]
     (or (get cell :computed) (get cell :value))))
 
-; Return a list of coords in string range
-; Ex: "b1:b4" -> [b1, b2, b3, b4]
+; Return a list of coord values in string range
+; Ex: "b1:b4" -> [12, 200, 5.5, 75]
 (defn unpack-range [_range all-values]
-  (let [values (s/split _range #":")
+  (let [values (cs/split _range #":")
         letter (first (first values))
         start-row (subs (first values) 1)
         end-row (subs (last values) 1)]
@@ -34,11 +34,11 @@
 ; Ex: [b1, b2, b3] -> [5, 3, 19]
 (defn get-coord-values [args all-values]
   (reduce (fn [acc el]
-            (if (s/includes? el ":")
-              (into acc (unpack-range (s/trim el) all-values))
+            (if (cs/includes? el ":")
+              (into acc (unpack-range (cs/trim el) all-values))
               ; Else
-              (if (> (count (str (get-coord-value all-values (s/trim el)))) 0)
-                (conj acc (js/parseFloat (get-coord-value all-values (s/trim el))))
+              (if (> (count (str (get-coord-value all-values (cs/trim el)))) 0)
+                (conj acc (js/parseFloat (get-coord-value all-values (cs/trim el))))
                 acc))) [] args))
 
 ; Cells (args) that should update when a formula is updated
@@ -46,15 +46,15 @@
 ; Formula map at key e1 will receive this vector so we know what cells to watch
 (defn get-formula-dependency-cells [args]
   (reduce (fn [acc el]
-            (if (s/includes? el ":")
-              (let [values (s/split el #":")
+            (if (cs/includes? el ":")
+              (let [values (cs/split el #":")
                     letter (first (first values))
                     start-row (subs (first values) 1)
                     end-row (subs (last values) 1)]
                 (into acc
                       (map #(str letter %)
                            (range (int start-row) (inc (int end-row))))))
-              (conj acc (s/trim el)
+              (conj acc (cs/trim el)
                     acc)))
           [] args))
 
@@ -62,11 +62,11 @@
 (defn compute-formula [formula all-values update-formula-cell-map coord]
   (if (> (count formula) 3)
     (if-let [op (get operations (keyword (subs formula 1 4)))]
-      (let [arg-start (inc (s/index-of formula "("))
-            arg-end (s/index-of formula ")")]
+      (let [arg-start (inc (cs/index-of formula "("))
+            arg-end (cs/index-of formula ")")]
         (when (and arg-start arg-end op)
           (let [args
-                (s/split (s/trim (subs formula arg-start arg-end)) #",")]
+                (cs/split (cs/trim (subs formula arg-start arg-end)) #",")]
             (update-formula-cell-map
              coord
              (get-formula-dependency-cells args))
